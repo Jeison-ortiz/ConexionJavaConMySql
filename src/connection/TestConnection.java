635f29ad7a203dd5854bd9c06000b425709105a7
Jeison Ortiz <ortizjeison4@gmail.com>
2023-02-25 21:51:39 -0500

    Refactor(connection): Se arregla la conexión y se optimiza, de tal forma que se cierra los objetos statement, connection y ResultSet

--- src/connection/TestConnection.java
+++ src/connection/TestConnection.java
@@ -1,10 +1,50 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class TestConnection {
 	
 	public static void main(String[] args) {
 		Conexion conexion = new Conexion();
-		conexion.conectar();
+		Connection connection = null;
+		Statement stament = null;
+		ResultSet rs = null;
+			
+		try {		
+			connection = conexion.conectar();
+			stament = connection.createStatement();
+			rs = stament.executeQuery("SELECT * FROM usuario");
+			
+			while(rs.next()) {
+				int idUsuario = rs.getInt(1);
+				String usuario = rs.getString(2);
+				String clave = rs.getString(3);			
+				System.out.println("id-> " + idUsuario + " usuario-> " + usuario + " clave " + clave);	
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null) {
+					rs.close();
+				}
+				
+				if(stament!=null) {
+					stament.close();
+				}
+				
+				if(connection!=null) {
+					connection.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
 	}
 	
 	
